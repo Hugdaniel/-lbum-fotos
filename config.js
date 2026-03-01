8e66832914e0903636b5ec20b54f7d1004308734
Daniel Barrios <85hbdaniel@gmail.com>
2026-02-28 21:09:34 -0300

    html y config de js

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    nombreEvento: "EL CUMPLE DE DIEGO",
+    personajeImg: "assets/diego-skin.png", // La imagen del nene que generamos
+    fondoImg: "assets/bg-mc.jpg", // La jungla que generamos
+    // Aquí pondrás los nombres de los archivos que subas a GitHub
+    fotos: [
+        "pics 1.jpg",
+        "pics 2.jpg",
+        "pics 3.jpg",
+        "pics 4.jpg",
+        "pics 5.jpg",   
+        "pics 6.jpg",
+        "pics 7.jpg",
+        "pics 8.jpg",
+        "pics 9.jpg",
+        "pics 10.jpg",
+    ]
+};
